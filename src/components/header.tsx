ad92d092433baeed3794a77e7b879d08dbcf0f2e
romeucajamba <romeucajamba@gmail.com>
2024-05-12 20:00:47 +0100

    feat:finish the table attendees

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,8 +8,8 @@ export function Header() {
             <img src={Icon} alt="icon_yhanko" />
 
             <nav className='flex items-center gap-5 py-5'>
-                <a href="#" target="_blank" className='font-medium text-sm text-zinc-400'>Events</a>    
-                <a href="#" target="_blank" className='font-medium text-sm'>Attendees</a>
+                <a href="#" target="_blank" className='font-medium text-sm text-zinc-400'>Eventos</a>    
+                <a href="#" target="_blank" className='font-medium text-sm'>Participantes</a>
             </nav>
 
         </header>
