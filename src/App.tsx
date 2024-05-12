cce8544f22b77b7213bb11722c4d9e0aef06d607
romeucajamba <romeucajamba@gmail.com>
2024-05-12 06:38:22 +0100

    feat:creating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
-
-function App() {
-
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+interface MyBottom {
+  text: string
 }
 
-export default App
+function Button(props: MyBottom){
+  console.log(props)
+  return <button className="bg-orange-400 h-10 px-3 font-medium rounded">{props.text}</button>
+}
+
+export function App() {
+
+  return (
+    <div className="flex gap-2">
+      <Button text="clica em mim!"/>
+      <Button text="clica em mim!"/>
+      <Button text="clica em mim!"/>
+      <Button text="clica em mim!"/>
+    </div>
+  )
+}
---
+++ src/components/attendee_list.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+export function AttendeeList(){
+    return(
+        <main>
+            <h1>Lista de participantes</h1>
+        </main>
+    )
+}
