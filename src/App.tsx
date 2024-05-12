437b883d7a125d8ede5c20eb8cdefbb318d0225c
romeucajamba <romeucajamba@gmail.com>
2024-05-12 17:03:31 +0100

    feat: creatd the new components, Header and Attendees

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-
-interface MyBottom {
-  text: string
-}
-
-function Button(props: MyBottom){
-  console.log(props)
-  return <button className="bg-orange-400 h-10 px-3 font-medium rounded">{props.text}</button>
-}
+import { Header} from './components/header';
+import { AttendeeList } from './components/attendee_list';
 
 export function App() {
 
   return (
-    <div className="flex gap-2">
-      <Button text="clica em mim!"/>
-      <Button text="clica em mim!"/>
-      <Button text="clica em mim!"/>
-      <Button text="clica em mim!"/>
+    <div className='max-w-[1216px] mx-auto'>
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,20 @@
+import Icon from '../../public/Rectangle 592.png';
+
+
 export function Header() {
     return(
-        <header></header>
+        <header>
+            <img src={Icon} alt="icon_yhanko" />
+            <nav>
+                <ul>
+                    <li>
+                        <a href="http://" target="_blank">Events</a>
+                    </li>
+                    <li>
+                        <a href="http://" target="_blank">Attendees</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
