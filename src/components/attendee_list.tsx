6c3b1d3b58693146b50ce1f7ce22d90aae16aed5
romeucajamba <romeucajamba@gmail.com>
2024-05-14 12:41:20 +0100

    feat: implemented pagination and disabled button

--- src/components/attendee_list.tsx
+++ src/components/attendee_list.tsx
@@ -7,17 +7,43 @@ import { TableRow } from './tableRow';
 import { attendees} from '../data/attendeeList';
 import { pt} from 'date-fns/locale';
 import { formatRelative } from 'date-fns';
+import { ChangeEvent, useState } from 'react';
 
 
 export function AttendeeList(){
+    const [search, setsearch] = useState('')
+    const [page, setPage] = useState(0)
+    const totalPages = Math.ceil(attendees.length / 10)
+
+    function onSearchInput(event: ChangeEvent<HTMLInputElement>){
+        setsearch(event.target.value)
+    }
+
+    function goToFirstPage(){
+        setPage(1)
+    }
+
+    function goToNext(){
+        setPage(page + 1)
+    }
+
+    function goToPrevius(){
+        setPage(page - 1)
+    }
+
+    function goToLastPage(){
+        setPage(totalPages)
+    }
+
     return(
         <main className='flex flex-col gap-4'>
            <div className="flex items-center gap-3">
              <h1 className="text-2xl font-bold">Lista de participantes</h1>
              <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                 <Search className='size-4 text-[#610CE1]'/>
-                <input type="text" placeholder="pesquisar participante" className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" />
+                <input onChange={onSearchInput} type="text" placeholder="pesquisar participante" className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" />
              </div>
+                {search}
            </div>
 
         <Table>
@@ -37,7 +63,7 @@ export function AttendeeList(){
             </thead>
             <tbody>
                {
-                attendees.map(( attendee ) => {
+                attendees.slice(page, 10).map(( attendee ) => {
                     return(
                         <tr key={attendee.id} className='border-b border-white/10 hover:bg-white/5'>
                         <TdCell className='py-3 px-4 text-sm font-semibold text-zinc-300'>
@@ -66,23 +92,23 @@ export function AttendeeList(){
             <tfoot>
                 <tr>
                     <TdCell className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>
-                        Mostrando 10 de 228
+                        Mostrando 10 de {attendees.length}
                     </TdCell>
                     <TdCell className='text-right' colSpan={3}>
                        <div className='inline-flex items-center gap-8'>
-                            <span>página 1 de 23</span>
+                            <span>página {page} de {totalPages}</span>
 
                             <div className='flex gap-1.5'>
-                                <IconButton>
+                                <IconButton onClick={goToFirstPage} disabled={page === 1}>
                                     <ChevronsLeft className='size-4 text-white'/>
                                 </IconButton>
-                                <IconButton>
+                                <IconButton onClick={goToPrevius} disabled={page === 1}>
                                     <ChevronLeft className='size-4 text-white'/>
                                 </IconButton>
-                                <IconButton>
+                                <IconButton onClick={goToNext} disabled={page === totalPages}>
                                     <ChevronRight className='size-4 text-white'/>
                                 </IconButton>
-                                <IconButton>
+                                <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                                     <ChevronsRight className='size-4 text-white'/>
                                 </IconButton>
                             </div>
