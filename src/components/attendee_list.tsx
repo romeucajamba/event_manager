ddcb7b8f677bb72146c93340e840c67962fb298b
romeucajamba <romeucajamba@gmail.com>
2024-05-14 10:11:23 +0100

    feat:implement IconButton Component

--- src/components/attendee_list.tsx
+++ src/components/attendee_list.tsx
@@ -1,5 +1,5 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from 'lucide-react';
-
+import { IconButton } from './icon_button';
 
 
 
@@ -48,9 +48,9 @@ export function AttendeeList(){
                             <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atrás</td>
                             <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atrás</td>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>
-                                <button className='bg-black/20 border border-white/10 rounded-md px-1.5 py-1.5'>
-                                    <MoreHorizontal className='size-4 text-white'/>
-                                </button>
+                            <IconButton transparent>
+                                <MoreHorizontal className='size-4 text-white'/>
+                            </IconButton>
                             </th>
                         </tr>
                     )
@@ -68,18 +68,18 @@ export function AttendeeList(){
                             <span>página 1 de 23</span>
 
                             <div className='flex gap-1.5'>
-                                <button className='bg-white/10 border border-white/10 rounded-md px-1.5 py-1.5'>
+                                <IconButton>
                                     <ChevronsLeft className='size-4 text-white'/>
-                                </button>
-                                <button className='bg-white/10 border border-white/10 rounded-md px-1.5 py-1.5'>
+                                </IconButton>
+                                <IconButton>
                                     <ChevronLeft className='size-4 text-white'/>
-                                </button>
-                                <button className='bg-white/10 border border-white/10 rounded-md px-1.5 py-1.5'>
+                                </IconButton>
+                                <IconButton>
                                     <ChevronRight className='size-4 text-white'/>
-                                </button>
-                                <button className='bg-white/10 border border-white/10 rounded-md px-1.5 py-1.5'>
+                                </IconButton>
+                                <IconButton>
                                     <ChevronsRight className='size-4 text-white'/>
-                                </button>
+                                </IconButton>
                             </div>
                        </div>
                     </td>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import Icon from '../../public/Rectangle 592.png';
-
+import { NavLink } from './navLink';
 
 export function Header() {
     return(
@@ -7,9 +7,9 @@ export function Header() {
 
             <img src={Icon} alt="icon_yhanko" />
 
-            <nav className='flex items-center gap-5 py-5'>
-                <a href="#" target="_blank" className='font-medium text-sm text-zinc-400'>Eventos</a>    
-                <a href="#" target="_blank" className='font-medium text-sm'>Participantes</a>
+            <nav className='flex items-center gap-5 py-5'>   
+                <NavLink href="/eventos">Eventos</NavLink>
+                <NavLink href="/participantes">Participantes</NavLink>
             </nav>
 
         </header>
